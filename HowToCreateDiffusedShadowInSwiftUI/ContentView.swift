f555d74bc644901bc795203885fcd65f9996090c
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-09-24 19:43:13 +0300

    init

--- HowToCreateDiffusedShadowInSwiftUI/ContentView.swift
+++ HowToCreateDiffusedShadowInSwiftUI/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black.opacity(0.2).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Girl")
+                    .bold()
+                    .padding()
+                
+                ZStack(alignment: .leading) {
+                    
+                    Image("girl")
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                        .scaledToFill()
+                        .cornerRadius(20)
+                        .blendMode(.plusDarker)
+                        .blur(radius: 15)
+                        .offset(y: 20)
+                    
+                    Image("girl")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 300, height: 300)
+                        .cornerRadius(20)
+                }
+            }
+        }
     }
 }
 
