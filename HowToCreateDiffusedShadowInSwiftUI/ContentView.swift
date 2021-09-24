32a8837f907057720e987bd23ba942d0714b9294
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-09-24 19:27:41 +0300

    Initial Commit

---
+++ HowToCreateDiffusedShadowInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowToCreateDiffusedShadowInSwiftUI
+//
+//  Created by Ramill Ibragimov on 24.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HowToCreateDiffusedShadowInSwiftUI/HowToCreateDiffusedShadowInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateDiffusedShadowInSwiftUIApp.swift
+//  HowToCreateDiffusedShadowInSwiftUI
+//
+//  Created by Ramill Ibragimov on 24.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateDiffusedShadowInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
